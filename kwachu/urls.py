abead003b2a550a6afbbaf66893e8c9e0576ef6f
ramza007 <ramza007@gmail.com>
2018-05-18 11:41:31 +0300

    added home page

--- kwachu/urls.py
+++ kwachu/urls.py
@@ -3,8 +3,10 @@ main routes
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
+    url(r'^$', views.index),
     url(r'^admin/', admin.site.urls),
     url(r'^driver/', include('driver.urls')),
     url(r'^passanger/', include('passanger.urls')),
---
+++ kwachu/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def index(request):
+    html = f'''
+        <html>
+            <body>
+                <h1>Hello Welcome To Kwachu</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
