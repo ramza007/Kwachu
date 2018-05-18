1668a8843a5c24e2a83c3c723c346b699b2e97cc
ramza007 <ramza007@gmail.com>
2018-05-19 01:37:19 +0300

    modifies driver urls and added temlate file to project folder

--- driver/urls.py
+++ driver/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^driver',views.driverhome,name = 'home'),
+    url('^$',views.driverhome,name = 'home'),
     url(r'^new/driver/', views.new_driver, name="newdriver"),
     url(r'^login/driver/', views.driver_login, name="driverlogin"),
 ]
