cc51118b11e9154df085607ff9da5b539afcde5c
ramza007 <ramza007@gmail.com>
2018-05-20 17:23:48 +0300

    added modifications to home page

--- driver/urls.py
+++ driver/urls.py
@@ -5,6 +5,6 @@ urlpatterns=[
     url(r'^$',views.driverhome,name = 'home'),
     url(r'^new/', views.new_driver, name="newdriver"),
     url(r'^login/', views.driver_login, name="driverlogin"),
-    url( r'^profile/driver/(\d+)/(\d+)', views.driver_profile, name="driverProfile"),
+    url( r'^profile/(\d+)/(\d+)', views.driver_profile, name="driverProfile"),
     # url( r'^update/passenger/profile/(\d+)', views.update_passenger_profile, name="updatePassengerProfile"),
 ]
